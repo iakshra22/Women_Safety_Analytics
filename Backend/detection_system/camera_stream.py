6258600fb93c1638de20401bcf01b4049ca88d36
Akshra <akshradang@gmail.com>
2025-11-19 00:56:45 +0530

    Update camera_stream.py

--- Backend/detection_system/camera_stream.py
+++ Backend/detection_system/camera_stream.py
@@ -3,7 +3,7 @@ import cv2
 class CameraStream:
     def __init__(self, url):
         self.url = url
-        self.cap = cv2.VideoCapture(url)
+        self.cap = cv2.VideoCapture(http://10.172.160.23:8080/video)
 
     def get_frame(self):
         ret, frame = self.cap.read()
@@ -14,3 +14,4 @@ class CameraStream:
     def release(self):
         self.cap.release()
         cv2.destroyAllWindows()
+

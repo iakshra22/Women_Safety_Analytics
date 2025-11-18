f4f1d8452fd211445f6ea995c18b8950e963be68
Akshra <akshradang@gmail.com>
2025-11-19 00:56:17 +0530

    Update frame_processor.py

--- Backend/detection_system/frame_processor.py
+++ Backend/detection_system/frame_processor.py
@@ -3,7 +3,8 @@ import cv2
 class FrameProcessor:
     @staticmethod
     def preprocess(frame):
-        frame = cv2.resize(frame, (224, 224))  # Same size as model input
+        frame = cv2.resize(frame, (224, 224))  
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame = frame / 255.0
+
         return frame
